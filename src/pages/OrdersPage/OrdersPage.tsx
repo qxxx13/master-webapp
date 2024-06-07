df1b07cd7e7f688ead34740c74cc28c03492a108
Nikolay Nazarenko <redecv9@gmail.com>
2024-06-07 14:48:04 +0300

    test: tg button

--- src/pages/OrdersPage/OrdersPage.tsx
+++ src/pages/OrdersPage/OrdersPage.tsx
@@ -1,3 +1,14 @@
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
 export const OrdersPage = () => {
-    return <></>;
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <Button variant="outlined" onClick={() => navigate('/login')}>
+                Go to login
+            </Button>
+        </>
+    );
 };
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 
 import { LoginPage } from '../pages/LoginPage/LoginPage';
+import { OrdersPage } from '../pages/OrdersPage/OrdersPage';
+import { ProtectedRoute } from './ProtectedRoute';
 
 export const AppRouter = () => {
     return (
         <Routes>
             <Route path="/login" element={<LoginPage />} />
+            <Route
+                path="/"
+                element={
+                    <ProtectedRoute>
+                        <OrdersPage />
+                    </ProtectedRoute>
+                }
+            />
         </Routes>
     );
 };
